e180313ebb8bb3f9af6ab824386cbbb30d135cb2
HeyMax <heymaxytinfo@gmail.com>
2023-03-16 21:17:28 +0100

    Create aktenzeichen_generator.py

---
+++ aktenzeichen_generator.py
@@ -0,0 +1,8 @@
+import string
+import random
+from random import randint
+randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+y = randint (1, 100)
+z = randint (100, 300)
+print (randomUpperLetter,(y),('/'),(z))
+
